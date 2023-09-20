c50fc6e268be16856728d97e4daad4e2a7eed144
Fevzi Berk Çeliktaş <fevzi.celiktas@tedu.edu.tr>
2023-09-21 01:58:25 +0300

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose")
-mongoose.connect("mongodb://0.0.0.0:27017/OldTaxForm")
+mongoose.connect("mongodb+srv://Berqque:Database123321 @atlascluster.vt1is9m.mongodb.net/")
 .then(()=>{
     console.log("mongodb connected");
 })
